0f6116531341a380a235d89717f8ece96377302b
Ryan Tvenge <ryan@hoverboardstudios.com>
2019-04-09 20:20:26 -0700

    Fixing styles on backend

--- functions.php
+++ functions.php
@@ -6,4 +6,6 @@ function block_talk_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
+add_editor_style('style.css');
+
 // Register a new ACF Block: https://www.advancedcustomfields.com/resources/acf_register_block/
